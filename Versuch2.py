42821551ee8e9e2a51130373103b57904a785922
Alexander Helbok <alexander.helbok@hotmail.com>
2022-10-30 11:58:02 +0100

    Committed by TeXstudio

--- Versuch2.py
+++ Versuch2.py
@@ -22,7 +22,6 @@ print("\n")
 data = np.loadtxt("data/Versuch2_3.csv", delimiter=",", skiprows=1)
 # write data to pandas dataframe and skip first row
 df = pd.DataFrame(data, columns=["t", "F"])
-# 47.8 + 6
 # fit sine wave
 sta = 10199
 time = 33800
@@ -32,13 +31,12 @@ popt, pcov = curve_fit(sine, df["t"][20000:26000], df["F"][20000:26000])
 
 
 def sine2(t, f, phase):
-    return 0.10524441*np.sin(f*t + phase) - 2.00593556
+    return popt[0]*np.sin(f*t + phase) + popt[3]
 
 
 # popt = np.array([0.10524441, 8.40380255, -32.37785861, -2.00593556])
-popt2, pcov2 = curve_fit(sine2, df["t"][sta:time], df["F"][sta:time], p0=[8.4, -1])
+popt2, pcov2 = curve_fit(sine2, df["t"][sta:time], df["F"][sta:time], p0=[popt[1], popt[2]])
 # print(f"fit = {popt}, {popt2}, {np.sqrt(pcov2[0, 0])}")
-# plot first 10 seconds of data
 # plt.scatter(df["t"][sta:time], df["F"][sta:time], s=1, label="data")
 # # plot sine wave
 # plt.plot(df["t"][sta:time], sine2(df["t"][sta:time], *popt2), "r-", label="Fit")
@@ -66,24 +64,20 @@ sem = std / np.sqrt(len(df["F"]))
 
 F0 = unc.ufloat(mean, sem)
 F1 = unc.ufloat(popt[0], np.sqrt(pcov[0][0])) + unc.ufloat(popt[3], np.sqrt(pcov[3][3]))
-# print(popt[0]+popt[3])
-# print(F1/F0)
 T = unc.ufloat(4*np.pi/popt2[0], np.sqrt(pcov2[0, 0]), "T")
 print(f"T: {T:.1uS}")
+print(f"F0: {F0:.1uS}")
+print(f"F1: {F1:.1uS}")
 
-l1 = unc.ufloat(0.478, 0.002, "L")
-mid = unc.ufloat(0.06, 0.002, "L")
+faden = unc.ufloat(0.478, 0.002, "L")
+center = unc.ufloat(0.06, 0.002, "L")
 # calculate g from Pendulum
-# L = unc.ufloat(0.538, 0.001, "L")
-L = l1 + mid
+L = faden + center
 print(f"L:{L:.1uS}")
-# T = unc.ufloat(2*np.pi/popt[1], np.sqrt(pcov[1, 1]), "T")
 g1 = 4*(np.pi**2)*L/(T**2)
 
 # calculate g from first order pendulum
 theta = unp.arccos(F1/F0)
-print(f"F0: {F0:.1uS}")
-print(f"F1: {F1:.1uS}")
 print(f"theta: {theta*180/np.pi:.1uS}")
 g2 = g1/(1 + theta**2/16)
 print(f"g1: {g1:.1uS}")
@@ -99,7 +93,7 @@ print("\n")
 Wcoins = unp.uarray([2.3, 3.06, 3.92, 4.1], 0.01*np.array([2.3, 3.06, 3.92, 4.1]), )
 NCoins = np.array([2, 7, 3, 1])
 # calculate total weight
-Wtotal = unc.ufloat(0, 0)
+Wtotal = unc.ufloat(0, 0, "Wtotal")
 for i in range(len(Wcoins)):
     Wtotal += Wcoins[i] * NCoins[i]
 # print total weight and error
@@ -114,15 +108,23 @@ df = pd.DataFrame(data, columns=["t", "F"])
 mean = unp.nominal_values(df["F"]).mean()
 std = unp.std_devs(df["F"]).mean()
 sem = std / np.sqrt(len(df["F"]))
-print(mean, std)
+
 # set g to gravity in innsbruck
 g = 9.806
 # calculate force
+Fgem = unc.ufloat(mean, sem, "Fgem")
 F = -Wtotal * g/1000
+F1 = -Wtotal * g1/1000
 F2 = -Wtotal * g2/1000
-# compare force with mean of force data
-print(f"Force: {F:.1uS} Force data: {mean} difference: {F2:.1uS}")
 
+a1 = Fgem/F1
+a2 = Fgem/F2
+a = Fgem/F
+# contributions(a1)
+# compare force with mean of force data
+print(f"Force: {Fgem:.1uS} F1: {F1:.1uS} F2: {F2:.1uS}")
+print(f"a1: {a1:.1uS} a2: {a2:.1uS} a: {a:.1uS}")
+print(f"{g2/a:.1uS}")
 # plot every 20 data point as scatter and small point size
 # plot labels with units
 plt.scatter(df["t"][::20], df["F"][::20], s=1, label="data")
@@ -133,31 +135,3 @@ plt.ylabel("F [N]")
 plt.xlim(0, 12)
 plt.legend()
 plt.show()
-
-# %%
-# load data from csv and skip first row
-# load force data from csv
-data = np.loadtxt("data/Versuch2_3.csv", delimiter=",", skiprows=1)
-# write data to pandas dataframe and skip first row
-df = pd.DataFrame(data, columns=["t", "F"])
-# 47.8 + 6
-# fit sine wave
-sta = 20000
-time = 26000
-popt, pcov = curve_fit(sine, df["t"][sta:time], df["F"][sta:time])
-
-
-# plot first 10 seconds of data
-# plt.scatter(df["t"][sta:time], df["F"][sta:time], s=1, label="data")
-# # plot sine wave
-# plt.plot(df["t"][sta:time], sine(df["t"][sta:time], *popt), "r-", label="Fit")
-plt.scatter(df["t"], df["F"], s=1, label="data")
-plt.plot(df["t"], sine(df["t"], *popt), "r-", label="Fit")
-plt.xlabel("t [s]")
-plt.ylabel("F [N]")
-plt.xlim(0, 10)
-plt.legend()
-plt.show()
-
-T = unc.ufloat(2*np.pi/popt[1], np.sqrt(pcov[1, 1]), "T")
-print("T: ", T)

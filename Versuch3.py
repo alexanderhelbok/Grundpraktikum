43b6735a7a92ddb1877ee5cec219359aba402748
Alexander Helbok <alexander.helbok@hotmail.com>
2022-11-10 09:00:54 +0100

    Committed by TeXstudio

--- Versuch3.py
+++ Versuch3.py
@@ -10,20 +10,30 @@ for i in range(3):
     # write data to pandas dataframe
     df = pd.DataFrame(data, columns=["t", "a", "F"])
     # fill aerr and Ferr with 0.001
-    df["aerr"] = 0.001
+    df["aerr"] = 0.01
     df["Ferr"] = 0.006
     rate = 400
 
-    popt, pcov = curve_fit(const, df["t"][4*rate:12*rate], df["a"][4*rate:12*rate], sigma=df["aerr"][4*rate:12*rate], absolute_sigma=True)
-    popt2, pcov2 = curve_fit(const, df["t"][4*rate:12*rate], df["F"][4*rate:12*rate], sigma=df["Ferr"][4*rate:12*rate], absolute_sigma=True)
+    amean = df["a"][4*rate:12*rate].mean()
+    astd = df["a"][4*rate:12*rate].std()
+    fmean = df["F"][4*rate:12*rate].mean()
+    fstd = df["F"][4*rate:12*rate].std()
 
-    chi2 = chisq(const(df["t"][4*rate:12*rate], *popt), df["a"][4*rate:12*rate])
+    F = unc.ufloat(fmean - df["F"][0], np.sqrt(2)*fstd)
+    a = unc.ufloat(amean, np.sqrt(2)*astd)
+    print(f"F: {F:.1uS}")
+    print(f"a: {a:.1uS}")
+
+    # popt, pcov = curve_fit(const, df["t"][4*rate:12*rate], df["a"][4*rate:12*rate], sigma=df["aerr"][4*rate:12*rate], absolute_sigma=True)
+    # popt2, pcov2 = curve_fit(const, df["t"][4*rate:12*rate], df["F"][4*rate:12*rate], sigma=df["Ferr"][4*rate:12*rate], absolute_sigma=True)
+
+    # chi2 = chisq(const(df["t"][4*rate:12*rate], *popt), df["a"][4*rate:12*rate])
     # print(chisq(const(df["t"][4*rate:12*rate], *popt), df["F"][4*rate:12*rate], error=df["Ferr"][4*rate:12*rate], dof=len(df["t"][4*rate:12*rate])-1))
-    alpha = np.sqrt(chi2/(len(df["t"][4*rate:12*rate])-2))
-    print(alpha)
-    F = unc.ufloat(popt2[0], np.sqrt(pcov2[0][0]))
-    a = unc.ufloat(popt[0], np.sqrt(pcov[0][0]))
-    print(f"F: {F.s:.8f}")
+    # alpha = np.sqrt(chi2/(len(df["t"][4*rate:12*rate])-2))
+    # print(alpha)
+    # F = unc.ufloat(popt2[0], np.sqrt(pcov2[0][0]))
+    # a = unc.ufloat(popt[0], np.sqrt(pcov[0][0]))
+    # print(f"F: {F.s:.8f}")
 
     # F = unc.ufloat(np.round(popt2[0], 3), 0.006)
     # a = unc.ufloat(np.round(popt[0], 3), 0.02)
@@ -32,22 +42,22 @@ for i in range(3):
         # plot force, acceleration and F/a
         fig, (ax1, ax2, ax3) = plt.subplots(3, 1, sharex=True)
         ax1.scatter(df["t"], df["F"], s=0.5, color="black", label="Data")
-        ax1.plot(df["t"][3*rate:12*rate], line(df["t"][3*rate:12*rate], 0, *popt2), label="Fit", color="red", linewidth=0.75)
+        ax1.hlines(fmean, df["t"][3*rate], df["t"][12*rate], label="Fit", color="red", linewidth=0.75)
         ax1.set_ylabel("F [N]")
         ax1.legend(borderaxespad=1, markerscale=4)
         ax2.scatter(df["t"], df["a"], color="black", label="Data", s=0.5)
-        ax2.plot(df["t"][3*rate:12*rate], line(df["t"][3*rate:12*rate], 0, *popt), label="Fit", color="red", linewidth=0.75)
+        ax2.hlines(amean, df["t"][3*rate], df["t"][12*rate], label="Fit", color="red", linewidth=0.75)
         ax2.set_ylabel(r"a [m/s$^2$]")
         ax2.set_ylim(-11, -9)
         ax2.legend(borderaxespad=1, markerscale=4)
         ax3.scatter(df["t"], df["F"]/df["a"], label="Data", s=0.5, color="black")
-        ax3.plot(df["t"][3*rate:12*rate], line(df["t"][3*rate:12*rate], 0, *popt2)/line(df["t"][3*rate:12*rate], 0, *popt), label="Fit", color="red", linewidth=0.75)
+        ax3.hlines(m[i].n, df["t"][3*rate], df["t"][12*rate], label="Fit", color="red", linewidth=0.75)
         ax3.set_ylabel(r"F/a [kg]")
         ax3.set_xlabel("t [s]")
         ax3.legend(loc="lower right", borderaxespad=1,  markerscale=4)
         plt.xlim(0, 12)
-        fig.savefig("Graphics/Versuch3_1.eps", format="eps", transparent=True)
-        plt.show()
+        # fig.savefig("Graphics/Versuch3_1.eps", format="eps", transparent=True)
+        # plt.show()
 
     # print(f"a: {a:.1uS} F: {F:.1uS}")
     print(f"m{i+1}: {m[i]:.1uS}")
@@ -61,12 +71,13 @@ for i in range(3):
     # write data to pandas dataframe
     df = pd.DataFrame(data, columns=["t", "a", "F"])
     # fill aerr and Ferr with 0.001
-    df["aerr"] = 0.001
-    df["Ferr"] = 0.0025
+    df["aerr"] = astd
+    df["Ferr"] = fstd
 
     fit, fitcov = sine_fit(df["t"], df["a"], p0=[2000, 2200], min=1600)
 
-    w[i] = unc.ufloat(np.round(fit[1], 3), 0.01)
+    # w[i] = unc.ufloat(np.round(fit[1], 3), 0.1)
+    w[i] = unc.ufloat(fit[1], 0)
     T[i] = 2 * np.pi / w[i]
     if i == 2:
         plt.scatter(df["t"], df["a"], label="Data", s=0.5, color="black")
@@ -75,9 +86,12 @@ for i in range(3):
         plt.ylabel("a [m/s$^2$]")
         plt.legend(borderaxespad=1, loc="upper left", markerscale=4)
         plt.xlim(0, 6)
-        plt.savefig("Graphics/Versuch3_2.eps", format="eps", transparent=True)
-        plt.show()
-print(T)
+        # plt.savefig("Graphics/Versuch3_2.eps", format="eps", transparent=True)
+        # plt.show()
+
+for i in range(3):
+    print(f"T{i+1}: {T[i]:.1uS}")
+    print(f"w{i+1}: {w[i]:.1uS}")
 
 # %%
 # plot m2 and w
@@ -86,7 +100,7 @@ print(T)
 m2 = 1/unp.sqrt(m)
 k = w**2*m
 for i in range(3):
-    print(f"k{i+1} = {k[i]:.1uS} m2{i+1} = {m[i]:.1uS}")
+    print(f"k{i+1} = {k[i]:.1uS} m2{i+1} = {m[i]:.2uS}")
 # fit linear function to data
 popt, pcov = curve_fit(line, unp.nominal_values(m2), unp.nominal_values(w), sigma=unp.std_devs(w), absolute_sigma=True)
 popt2, pcov2 = curve_fit(affineline, unp.nominal_values(m2), unp.nominal_values(w), sigma=unp.std_devs(w), absolute_sigma=True)
@@ -101,11 +115,11 @@ plt.legend(loc="best")
 plt.show()
 
 # calc chi2
-chi1 = chisq(line(unp.nominal_values(m2), *popt), unp.nominal_values(w), error=unp.std_devs(w), dof=len(m2)-2)
-chi2 = chisq(affineline(unp.nominal_values(m2), *popt2), unp.nominal_values(w), error=unp.std_devs(w), dof=len(m2)-1)
-print(f"chi1 = {chi1:.2f} chi2 = {chi2:.2f}")
+# chi1 = chisq(line(unp.nominal_values(m2), *popt), unp.nominal_values(w), error=unp.std_devs(w), dof=len(m2)-2)
+# chi2 = chisq(affineline(unp.nominal_values(m2), *popt2), unp.nominal_values(w), error=unp.std_devs(w), dof=len(m2)-1)
+# print(f"chi1 = {chi1:.2f} chi2 = {chi2:.2f}")
 
-# %%
+        # %%
 # ========= 2 =========
 mass = m[2]
 kparallel = unp.uarray([0, 0, 0], [0, 0, 0])

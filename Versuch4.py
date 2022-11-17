e84f0ff5052ec44f312eda412641f0982e393948
Alexander Helbok <alexander.helbok@hotmail.com>
2022-11-17 17:19:10 +0100

    Committed by TeXstudio

--- Versuch4.py
+++ Versuch4.py
@@ -2,11 +2,16 @@ from Source import *
 
 # %%
 # load data
-# data = np.loadtxt("/home/taco/Documents/Grundpraktikum/data/Versuch1_1.csv", delimiter=",", skiprows=1)
+data = np.loadtxt("/home/taco/Documents/Grundpraktikum/data/Versuch1_1.csv", delimiter=",", skiprows=1)
 # create dataframe
-# df = pd.DataFrame(data, columns=["t", "x", "v", "a"])
+df = pd.DataFrame(data, columns=["t", "I_light", "I_sound"])
 
-x = np.linspace(1, 100, 1000000)
+df["I_light_err"] = 0
+df["I_sound_err"] = 0
+
+
+# %%
+x = np.linspace(1, 100, 100000)
 y = sine(x, 1, 1, 0, 0) + sine(x, 2, 3, 0, 0)
 
 print("x: ", x)

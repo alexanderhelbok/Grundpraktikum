a36f788dd6783cce2626011a5db30f0abe26f076
Alexander Helbok <alexander.helbok@hotmail.com>
2022-11-28 17:48:00 +0100

    Committed by TeXstudio

--- Versuch5.py
+++ Versuch5.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from Source import *
 # %%
 R1, R2 = unc.ufloat(10000, 10, "R1"), unc.ufloat(10000, 10, "R2")
@@ -72,8 +74,7 @@ for i in range(1, 4):
     mean2, std2 = df["V"][int(-4.5*rate):].mean(), df["V"][int(-4.5*rate):].std()
     V1 = unc.ufloat(mean1, std1, "V1")
     V2 = unc.ufloat(mean2, std2, "V2")
-    offset = np.abs((V1 + V2)/2)
-    V = V1 + offset
+    V = (np.abs(V1) + np.abs(V2))/2
     print(f"A{i}:")
     if i == 1:
         I1 = np.abs(V / Shunt)
@@ -81,7 +82,7 @@ for i in range(1, 4):
         print(f"V1= {V + R1 * I1 / 1000:.1uS}V: Videal = {R1 * Iideal1 / 1000:.1uS} V")
         print(f"I = {I1:.1uS} mA: Iideal = {Iideal1:.1uS} mA")
     elif i == 2:
-        V2 = V1 + offset
+        V2 = V
         I2 = np.abs(V / Shunt)
         Iideal2 = (Violab - R1*Iideal1/1000)/R2*1000
         print(f"V2 = {(Shunt+R2)*I2/1000:.1uS}V: Videal = {R2*Iideal2/1000:.1uS} V")
@@ -106,4 +107,50 @@ print(f"U3 = {U3:.1uS} V: U3ideal = {U3ideal:.1uS} V")
 print(f"U4 = {U4:.1uS} V: U4ideal = {U4ideal:.1uS} V")
 
 # %%
-for i in range
+# load data
+df = pd.read_csv("data/Versuch5_8.csv")
+# rename columns
+df.columns = ["t", "V"]
+rate = get_polling_rate(df)
+
+end, endstd = df["V"][0:int(4.5*rate)].mean(), df["V"][0:int(4.5*rate)].std()
+mid, midstd = df["V"][int(-4.5*rate):].mean(), df["V"][int(-4.5*rate):].std()
+mid, end = unc.ufloat(mid, midstd, "mid"), unc.ufloat(end, endstd, "end")
+
+U1 = end
+U2 = Violab - end
+U3 = mid - end
+U4 = Violab - mid
+print(f"U1 = {U1:.1uS} V")
+print(f"U2 = {U2:.1uS} V")
+print(f"U3 = {U3:.1uS} V")
+print(f"U4 = {U4:.1uS} V")
+
+# %%
+a = np.array([0, 5, 9.5, 14.5, 20.7, 24.4, 30.5, 35.5, 40, 45, 50, -1])
+b = np.array([0, 5, 10, 15, 18, 23, 30, 35, 41, 46, 53, -1])
+c = np.array([0, 5, 10, 15, 19, 24, 32, 37, 45, 50, 58, -1])
+# create 3x3 array
+V = unp.umatrix(np.empty((3, 3)), np.empty((3, 3)))
+for (i, j) in zip(range(1, 4), [a, b, c]):
+    # load data
+    df = pd.read_csv(f"data/Versuch5_{i+8}.csv")
+    # rename columns
+    df.columns = ["t", "V"]
+    rate = get_polling_rate(df)
+    # print(len(j))
+    for index in range(len(j)//4):
+        # print(index)
+        mean1, meanstd1 = df["V"][int(j[index*4]*rate):int(j[index*4+1]*rate)].mean(), df["V"][int(j[index*4]*rate):int(j[index*4+1]*rate)].std()
+        mean2, meandstd2 = df["V"][int(j[index*4+2]*rate):int(j[index*4+3]*rate)].mean(), df["V"][int(j[index*4+2]*rate):int(j[index*4+3]*rate)].std()
+        V1 = unc.ufloat(mean1, meanstd1, "V1")
+        V2 = unc.ufloat(mean2, meandstd2, "V2")
+        V[i-1, index] = (np.abs(V1) + np.abs(V2))/2
+        # print(V[i-1, index])
+
+U1 = (V[0, 0] + V[0, 1] + V[0, 2])/3
+U2 = (V[1, 0] + V[1, 1] + V[1, 2])/3
+U3 = (V[2, 0] + V[2, 1] + V[2, 2])/3
+print(f"U1 = {U1:.1uS} V")
+print(f"U2 = {U2:.1uS} V")
+print(f"U3 = {U3:.1uS} V")
